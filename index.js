a33f451fc249d9f1a8437c281cf809ac6d41e770
asqarbek olimov <88034288+asqarbekolimov@users.noreply.github.com>
2023-05-02 14:48:09 +0500

    Dynamic UI

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages", "about.html"));
+  res.render("about");
 });
 
 app.get("/contact", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages", "contact.html"));
+  res.render("contact");
 });
 
 app.get("/post", (req, res) => {
